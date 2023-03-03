49f6b42ec71b58473e636ceb1a11050ac1765740
Your Name <abbasovelshen@mail.ru>
2023-03-03 22:34:31 +0400

    sdcsd

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useRef } from 'react'
 import { useState } from 'react';
 import './App.css';
 
@@ -10,9 +11,8 @@ function App() {
     city: "",
     country: ""
     }
-     
-   
   })
+  let[table,tableSet]=useState()
   let [check,setCheck]=useState(false)
   let [check1,setCheck1]=useState(false)
   let [check2,setCheck2]=useState(false)
@@ -25,19 +25,30 @@ fetch('https://northwind.vercel.app/api/suppliers',{
       'Content-type': 'application/json'
     },
     body: JSON.stringify(data)
-  
   })
     }else{
       alert("məlumat düzgün deyil")
     }
     
   }
-  console.log(data);
+  let slide=useRef()
+  let getData=(e)=>{
+    e.preventDefault() 
+    slide.current.style.left="0px";
+    fetch('https://northwind.vercel.app/api/suppliers')
+    .then((Promise)=>{
+      return Promise.json()
+      })
+      .then((obj)=>{
+        tableSet(obj)
+      })
+  }
+  console.log(table);
   let [conpani, setConpani] = useState("")
   let [contac, setContac] = useState("")
   let [city, setCity] = useState("")
   let [country, setCountry] = useState("")
-
+  
   return (
     <div className="App">
 
@@ -46,23 +57,55 @@ fetch('https://northwind.vercel.app/api/suppliers',{
         <div className='secondDiv'>
         <form>
         <label htmlFor="fname">Conpani name:</label>
-        <input required onChange={(e) => e.target.value.length < 5 ?setConpani("min length 5"):setData({...data,conpaniName:e.target.value})&setConpani('')&setCheck(true) } type="text"  />
+        <input required onChange={(e) => e.target.value.length < 5 ?setConpani("min length 5"):
+        setData({...data,conpaniName:e.target.value})&setConpani('')&setCheck(true) } type="text"  />
         <p>{conpani}</p>
         <label htmlFor="lname">Contac name:</label>
-        <input required onChange={(e) => e.target.value.includes("@")?setContac("dont write @"): setData({...data,contacName:e.target.value})&setContac("")&setCheck1(true) } type="text"  />
+        <input required onChange={(e) => e.target.value.includes("@")?setContac("dont write @"):
+         setData({...data,contacName:e.target.value})&setContac("")&setCheck1(true) } type="text"  />
         <p>{contac}</p>
         <label htmlFor="contacTitle">Contac Title:</label>
-        <input required className='contacTitle' onChange={(e)=>setData({...data,contactTitle:e.target.value.toUpperCase()})} type="text" id="contacTitle" name="contacTitle" /><br/>
+        <input required className='contacTitle' 
+        onChange={(e)=>setData({...data,contactTitle:e.target.value.toUpperCase()})} type="text" id="contacTitle" name="contacTitle" /><br/>
         <label htmlFor="city">City:</label>
-        <input required  onChange={(e) => e.target.value.includes(" ")?setCity("empty"): setData({...data,adress:{city:e.target.value}})&setCity("")&setCheck2(true) } type="text" id="city"  />
+        <input required  onChange={(e) => e.target.value.includes(" ")?setCity("empty"):
+         setData({...data,adress:{city:e.target.value}})&setCity("")&setCheck2(true) } type="text" id="city"  />
         <p>{city}</p>
         <label htmlFor="country">Country:</label>
-        <input required onChange={(e) => e.target.value.includes(" ")?setCountry("empty"): setData({...data,adress:{...data.adress,country:e.target.value}})&setCity("")&setCheck3(true) } type="text" id="country" />
+        <input required onChange={(e) => e.target.value.includes(" ")?setCountry("empty"):
+         setData({...data,adress:{...data.adress,country:e.target.value}})&setCity("")&setCheck3(true) } type="text" id="country" />
         <p>{country}</p>
-        <button onClick={(e)=>postData()}>Send</button>
+          <button onClick={(e)=>postData()}>Send</button>
       </form>
         </div>
-        
+        <button className='getDAta' onClick={(e)=>getData(e)}>Get</button>
+      </div>
+      <div className='tablediv' ref={slide}>
+        {
+      table?<div>
+        <table>
+          <thead>
+            <tr>
+            <th>ID</th>
+            <th>Company Name</th>
+            <th>Contact Name</th>
+            <th>Contact Title</th>
+            <th>City</th>
+            <th>Country</th>
+          </tr>
+          </thead>
+          <tbody>
+            {table?.map((item,index)=><tr key={index}>
+              <td>{item.id}</td>
+              <td>{item.companyName}</td>
+              <td>{item.contactName}</td>
+              <td>{item.contactTitle}</td>
+              {/* <td>{item.address.city}</td> */}
+              {/* <td>{item.address.country}</td> */}
+              </tr>)}
+          </tbody>
+        </table>
+      </div>:null }
       </div>
       
     </div>
